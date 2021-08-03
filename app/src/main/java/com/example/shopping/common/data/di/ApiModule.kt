28f0729a6375a3e47b00061ee5b0e475fce63170
sourabh <nick.tdr19@gmail.com>
2021-08-03 12:57:04 +0530

    added di module for api

---
+++ app/src/main/java/com/example/shopping/common/data/di/ApiModule.kt
@@ -0,0 +1,55 @@
+package com.example.shopping.common.data.di
+
+import com.example.shopping.common.data.api.ApiConstants
+import com.example.shopping.common.data.api.ClientsApi
+import com.example.shopping.common.data.api.interceptors.LoggingInterceptor
+import com.example.shopping.common.data.api.interceptors.NetworkStatusInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApiModule {
+    @Provides
+    @Singleton
+    fun provideApi(builder: Retrofit.Builder): ClientsApi {
+        return builder
+            .build()
+            .create(ClientsApi::class.java)
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl(ApiConstants.BASE_CLIENT_ENDPOINT)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        networkStatusInterceptor: NetworkStatusInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(networkStatusInterceptor)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun provideHttpLoggingInterceptor(loggingInterceptor: LoggingInterceptor): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor(loggingInterceptor)
+
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return interceptor
+    }
+}
