b2e5daa0407c513e410b0ea7d4b656e7ba745aa4
sourabh <nick.tdr19@gmail.com>
2021-08-03 15:15:11 +0530

    added nav graph

---
+++ app/src/main/java/com/example/shopping/displayclients/presentation/ClientsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.shopping.displayclients.presentation
+
+import androidx.fragment.app.Fragment
+
+class ClientsFragment : Fragment() {
+}
---
+++ app/src/main/java/com/example/shopping/products/presentation/ProductsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.shopping.products.presentation
+
+import androidx.fragment.app.Fragment
+
+class ProductsFragment : Fragment() {
+}
