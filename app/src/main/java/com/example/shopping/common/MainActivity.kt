3d812439c6c55f04fe798ee8df3446528e3c5645
sourabh <nick.tdr19@gmail.com>
2021-08-03 11:04:25 +0530

    setup project structure by package by feature

--- app/src/main/java/com/example/shopping/common/MainActivity.kt
+++ app/src/main/java/com/example/shopping/common/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shopping
+package com.example.shopping.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shopping.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
