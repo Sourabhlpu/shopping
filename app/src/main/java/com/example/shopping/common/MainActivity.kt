fd7b180dd9621471f7c6b26a1871ff9a677c365a
sourabh <nick.tdr19@gmail.com>
2021-08-03 15:22:00 +0530

    added bottom navigation with nav controller in main activity

--- app/src/main/java/com/example/shopping/common/MainActivity.kt
+++ app/src/main/java/com/example/shopping/common/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.shopping.common
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.shopping.R
+import com.example.shopping.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(topLevelDestinationIds = setOf(R.id.clients, R.id.products))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
