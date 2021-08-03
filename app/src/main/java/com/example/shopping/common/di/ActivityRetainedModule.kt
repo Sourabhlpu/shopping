a354c8dd2a9d3493933f76507f67bfd68098cee0
sourabh <nick.tdr19@gmail.com>
2021-08-03 17:05:54 +0530

    displayed the list of clients

--- app/src/main/java/com/example/shopping/common/di/ActivityRetainedModule.kt
+++ app/src/main/java/com/example/shopping/common/di/ActivityRetainedModule.kt
@@ -1,5 +1,7 @@
 package com.example.shopping.common.di
 
+import com.example.shopping.common.data.ShoppingAppClientsRepository
+import com.example.shopping.common.domain.repositories.ClientRepository
 import com.example.shopping.common.utils.CoroutineDispatcherProvider
 import com.example.shopping.common.utils.DispatchersProvider
 import dagger.Binds
@@ -7,12 +9,19 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import io.reactivex.disposables.CompositeDisposable
 
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
 abstract class ActivityRetainedModule {
+
+    @Binds
+    @ActivityRetainedScoped
+    abstract fun bindAnimalsRepository(repository: ShoppingAppClientsRepository) :
+            ClientRepository
+
     @Binds
     abstract fun bindDispatchersProvider(dispatchersProvider: CoroutineDispatcherProvider):
             DispatchersProvider
