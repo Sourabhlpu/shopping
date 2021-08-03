42658d6fb8ecb853ddca2eb2f6aca6bd78734755
sourabh <nick.tdr19@gmail.com>
2021-08-03 16:52:32 +0530

    observing and updating ui on state changes

--- app/src/main/java/com/example/shopping/displayclients/presentation/ClientsFragment.kt
+++ app/src/main/java/com/example/shopping/displayclients/presentation/ClientsFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.shopping.R
 import com.example.shopping.common.presentation.ClientsAdapter
+import com.example.shopping.common.presentation.Event
 import com.example.shopping.databinding.FragmentClientsBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ClientsFragment : Fragment() {
     private val binding get() = _binding!!
@@ -29,15 +33,16 @@ class ClientsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUI()
-        requestInitialAnimalList()
+        requestInitialClientList()
     }
 
     private fun setupUI() {
         val adapter = createAdapter()
         setupRecyclerView(adapter)
+        observeViewStateUpdates(adapter)
     }
 
-    private fun requestInitialAnimalList() {
+    private fun requestInitialClientList() {
         viewModel.onEvent(ClientsEvent.RequestInitialClientList)
     }
 
@@ -49,8 +54,42 @@ class ClientsFragment : Fragment() {
         }
     }
 
+    private fun observeViewStateUpdates(adapter: ClientsAdapter) {
+        viewModel.state.observe(viewLifecycleOwner) {
+            updateScreenState(it, adapter)
+        }
+    }
+
+    private fun updateScreenState(
+        state: ClientsViewState,
+        adapter: ClientsAdapter
+    ) {
+        binding.progressBar.isVisible = state.loading
+        adapter.submitList(state.clients)
+        handleNoMoreAnimalsNearby(state.noMoreClients)
+        handleFailures(state.failure)
+    }
+
     private fun createAdapter(): ClientsAdapter {
         return ClientsAdapter()
+    }
+
+    private fun handleNoMoreAnimalsNearby(noMoreClients: Boolean) {
+
+    }
+
+    private fun handleFailures(failure: Event<Throwable>?) {
+        val unhandledFailure = failure?.getContentIfNotHandled() ?: return
+        val fallbackMessage = getString(R.string.an_error_occurred)
+        val snackbarMessage = if (unhandledFailure.message.isNullOrEmpty()) {
+            fallbackMessage
+        }
+        else {
+            unhandledFailure.message!!
+        }
+        if (snackbarMessage.isNotEmpty()) {
+            Snackbar.make(requireView(), snackbarMessage, Snackbar.LENGTH_SHORT).show()
+        }
     }
 
 }
