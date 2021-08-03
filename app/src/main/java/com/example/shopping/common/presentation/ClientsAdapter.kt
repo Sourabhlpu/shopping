d7f8409900954586604b50dba097bf825df591bd
sourabh <nick.tdr19@gmail.com>
2021-08-03 16:39:05 +0530

    added adapter for clients

---
+++ app/src/main/java/com/example/shopping/common/presentation/ClientsAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.shopping.common.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shopping.common.presentation.model.UIClient
+import com.example.shopping.databinding.RecyclerViewClientItemBinding
+
+class ClientsAdapter: ListAdapter<UIClient, ClientsAdapter.ClientsViewHolder>(ITEM_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClientsViewHolder {
+        val binding = RecyclerViewClientItemBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ClientsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ClientsViewHolder, position: Int) {
+        val item: UIClient = getItem(position)
+
+        holder.bind(item)
+    }
+
+    inner class ClientsViewHolder(
+        private val binding: RecyclerViewClientItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: UIClient) {
+            binding.name.text = item.name
+            binding.gender.text = item.gender
+        }
+    }
+}
+
+private val ITEM_COMPARATOR = object : DiffUtil.ItemCallback<UIClient>() {
+    override fun areItemsTheSame(oldItem: UIClient, newItem: UIClient): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: UIClient, newItem: UIClient): Boolean {
+        return oldItem == newItem
+    }
+}
