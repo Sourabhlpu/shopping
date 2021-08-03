4b435c1f8eed17aadd9c519ae80f74be0b6c6967
sourabh <nick.tdr19@gmail.com>
2021-08-03 16:42:07 +0530

    added ui setup for clients fragment

--- app/src/main/java/com/example/shopping/displayclients/presentation/ClientsFragment.kt
+++ app/src/main/java/com/example/shopping/displayclients/presentation/ClientsFragment.kt
@@ -7,8 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.shopping.common.presentation.ClientsAdapter
 import com.example.shopping.databinding.FragmentClientsBinding
-import com.google.android.material.snackbar.Snackbar
 
 class ClientsFragment : Fragment() {
     private val binding get() = _binding!!
@@ -28,6 +28,24 @@ class ClientsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        val adapter = createAdapter()
+        setupRecyclerView(adapter)
+    }
+
+    private fun setupRecyclerView(clientsAdapter: ClientsAdapter) {
+        binding.clientsRecyclerView.apply {
+            adapter = clientsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun createAdapter(): ClientsAdapter {
+        return ClientsAdapter()
     }
 
 }
